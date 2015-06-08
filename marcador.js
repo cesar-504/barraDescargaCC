e16a214213181a601d2dd3a3e222800ec8b7e7cd
cesar-504 <cesar-504@hotmail.com>
2015-06-08 01:09:11 -0500

    link script arreglado

--- marcador.js
+++ marcador.js
@@ -1,1 +1,6 @@
-javascript: (function() { var s = document.createElement('script');  s.type = 'text/javascript';  s.src = 'https://github.com/cesar-504/botonDescarga/blob/master/botonDescarga.js';    document.body.insertBefore(s, document.body.firstChild); })();
+javascript: (function() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://raw.githubusercontent.com/cesar-504/botonDescarga/master/botonDescarga.js';
+    document.body.insertBefore(s, document.body.firstChild);
+})();
