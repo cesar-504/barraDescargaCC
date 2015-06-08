98d5b61afc3d33474c80a5f7f66b9ce3a8e3c128
cesar-504 <cesar-504@hotmail.com>
2015-06-08 01:14:36 -0500

    link scrpt arreglado 2

--- marcador.js
+++ marcador.js
@@ -1,6 +1,6 @@
 javascript: (function() {
     var s = document.createElement('script');
     s.type = 'text/javascript';
-    s.src = 'https://raw.githubusercontent.com/cesar-504/botonDescarga/master/botonDescarga.js';
+    s.src = 'https://cdn.rawgit.com/cesar-504/botonDescarga/master/botonDescarga.js';
     document.body.insertBefore(s, document.body.firstChild);
 })();
